042ef9f93ae630eed9c4a748dc83b48cf251b83b
Rudimar Ronsoni <rudimar@outlook.com>
2026-04-12 17:14:29 +0200

    WIP: Phase 2 - Started kernel/ naming correction
    Created Linux-style headers:
    - task.h -> task_struct, sighand_struct
    - signal.h -> sighand_struct
    
    Note: Current state is broken - signal_ownership.md content was incorrectly written to task.h.
    Need to restore from backup and proceed more carefully.
    
    Status:
    - fdtable.h: COMPLETE (Linux-shaped)
    - fdtable.c: COMPLETE (Linux-shaped)
    - select.h: COMPLETE (new owner header)
    - eventpoll.h: COMPLETE (new owner header)
    - signal.h: IN PROGRESS (types written, content error)
    - task.h: IN PROGRESS (types written, content error)
    - IXLandSystem: BUILDS but IXLandSystem-KUnit fails

--- kernel/signal.h
+++ kernel/signal.h
@@ -1,5 +1,5 @@
-#ifndef IXLAND_SIGNAL_INTERNAL_H
-#define IXLAND_SIGNAL_INTERNAL_H
+#ifndef SIGNAL_H
+#define SIGNAL_H
 
 #ifndef _POSIX_C_SOURCE
 #define _POSIX_C_SOURCE 200809L
@@ -11,68 +11,80 @@
 
 #include "task.h"
 
-/* Include public signal types */
-#include "../include/ixland/ixland_signal.h"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /* Signal queue entry */
-typedef struct ixland_sigqueue_entry {
+typedef struct sigqueue_entry {
     int sig;
     siginfo_t info;
-    struct ixland_sigqueue_entry *next;
-} ixland_sigqueue_entry_t;
+    struct sigqueue_entry *next;
+} sigqueue_entry_t;
 
 /* Signal queue */
-typedef struct ixland_sigqueue {
-    ixland_sigqueue_entry_t *head;
-    ixland_sigqueue_entry_t *tail;
+typedef struct sigqueue {
+    sigqueue_entry_t *head;
+    sigqueue_entry_t *tail;
     int count;
     pthread_mutex_t lock;
-} ixland_sigqueue_t;
+} sigqueue_t;
 
-/* Signal handling state */
-struct ixland_sighand {
-    struct sigaction action[IXLAND_NSIG];
+/* Signal handling state (Linux-style sighand_struct) */
+struct sighand_struct {
+    struct sigaction action[NSIG];
     sigset_t blocked;
     sigset_t pending;
-    ixland_sigqueue_t queue;
+    sigqueue_t queue;
     atomic_int refs;
 };
 
-ixland_sighand_t *ixland_sighand_alloc(void);
-void ixland_sighand_free(ixland_sighand_t *sighand);
-ixland_sighand_t *ixland_sighand_dup(ixland_sighand_t *parent);
+/* Sighand allocation and management */
+struct sighand_struct *alloc_sighand(void);
+void free_sighand(struct sighand_struct *sighand);
+struct sighand_struct *dup_sighand(struct sighand_struct *parent);
 
-int ixland_sigaction(int sig, const struct sigaction *act, struct sigaction *oldact);
-int ixland_kill(pid_t pid, int sig);
-int ixland_killpg(pid_t pgrp, int sig);
-int ixland_sigprocmask(int how, const sigset_t *set, sigset_t *oldset);
-int ixland_sigpending(sigset_t *set);
-int ixland_sigsuspend(const sigset_t *mask);
-int ixland_sigqueue(pid_t pid, int sig, const union sigval value);
-int ixland_sigtimedwait(const sigset_t *set, siginfo_t *info, const struct timespec *timeout);
+/* Signal actions */
+int do_sigaction(int sig, const struct sigaction *act, struct sigaction *oldact);
 
-/* Signal handler installation (simplified interface) */
-typedef void (*ixland_sighandler_t)(int);
-ixland_sighandler_t ixland_signal(int signum, ixland_sighandler_t handler);
+/* Signal sending */
+int do_kill(pid_t pid, int sig);
+int do_killpg(pid_t pgrp, int sig);
+
+/* Signal masking */
+int do_sigprocmask(struct sighand_struct *sighand, int how, const sigset_t *set, sigset_t *oldset);
+int do_sigpending(struct sighand_struct *sighand, sigset_t *set);
+int do_sigsuspend(const sigset_t *mask);
+
+/* Signal queuing and waiting */
+int do_sigqueue(pid_t pid, int sig, const union sigval value);
+int do_sigtimedwait(const sigset_t *set, siginfo_t *info, const struct timespec *timeout);
+
+/* Simplified handler installation */
+typedef void (*sighandler_t)(int);
+sighandler_t do_signal(int signum, sighandler_t handler);
 
 /* Signal to current process */
-int ixland_raise(int sig);
+int do_raise(int sig);
 
 /* Alarm timer */
-unsigned int ixland_alarm(unsigned int seconds);
+unsigned int do_alarm(unsigned int seconds);
 
 /* Wait for signal */
-int ixland_pause(void);
+int do_pause(void);
 
-void ixland_signal_init(void);
-void ixland_signal_deinit(void);
+/* Initialization (internal use) */
+void signal_init(void);
+void signal_deinit(void);
+
+/* Internal signal delivery (called by task.c) */
+void force_sig(int sig, struct task_struct *task);
+
+/* Signal dispatch (internal) */
+void __apply_signal_to_task(struct task_struct *task, int sig);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif /* SIGNAL_H */
--- kernel/task.h
+++ kernel/task.h
@@ -1,5 +1,5 @@
-#ifndef IXLAND_TASK_H
-#define IXLAND_TASK_H
+#ifndef TASK_H
+#define TASK_H
 
 #include <pthread.h>
 #include <signal.h>
@@ -9,84 +9,91 @@
 #include <sys/types.h>
 #include <time.h>
 
+#include "../fs/fdtable.h"
+#include "../fs/vfs.h"
+#include "signal.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include "../include/ixland/ixland_types.h"
-
-#define IXLAND_MAX_NAME 256
-#define IXLAND_NSIG 64
-#define IXLAND_MAX_FD 256
-#define IXLAND_MAX_ARGS 256
-#define IXLAND_MAX_TASKS 1024
+#define TASK_COMM_LEN 16
+#define TASK_MAX_ARGS 256
+#define TASK_MAX_TASKS 1024
 
 #ifndef RLIMIT_NLIMITS
 #define RLIMIT_NLIMITS 16
 #endif
 
-typedef _Atomic pid_t atomic_pid_t;
+/* Forward declarations */
+struct task_struct;
+struct tty_struct;
+struct mm_struct;
+struct exec_image;
 
-typedef struct ixland_task ixland_task_t;
-typedef struct ixland_files ixland_files_t;
-typedef struct ixland_fs ixland_fs_t;
-typedef struct ixland_sighand ixland_sighand_t;
-typedef struct ixland_tty {
-    int tty_id;
+/* Task states - Linux-style */
+#define TASK_RUNNING 0
+#define TASK_INTERRUPTIBLE 1
+#define TASK_UNINTERRUPTIBLE 2
+#define TASK_STOPPED 4
+#define TASK_ZOMBIE 8
+#define TASK_DEAD 16
+
+/* TTY structure - canonical owner */
+struct tty_struct {
+    int index;
     pid_t foreground_pgrp;
     atomic_int refs;
-} ixland_tty_t;
+};
 
-typedef struct ixland_mm_emu {
+/* MM structure - canonical owner */
+struct mm_struct {
     void *exec_image_base;
     size_t exec_image_size;
-} ixland_mm_emu_t;
+    struct address_space *vma_addr_space;
+};
 
-typedef enum {
-    IXLAND_IMAGE_NONE = 0,
-    IXLAND_IMAGE_NATIVE,
-    IXLAND_IMAGE_WASI,
-    IXLAND_IMAGE_SCRIPT
-} ixland_image_type_t;
+/* Exec image types */
+enum {
+    EXEC_IMAGE_NONE = 0,
+    EXEC_IMAGE_NATIVE,
+    EXEC_IMAGE_WASI,
+    EXEC_IMAGE_SCRIPT,
+};
 
-typedef int (*ixland_native_entry_t)(struct ixland_task *task, int argc, char **argv, char **envp);
+/* Exec image entry */
+typedef int (*native_entry_t)(struct task_struct *task, int argc, char **argv, char **envp);
 
-typedef struct ixland_exec_image {
-    ixland_image_type_t type;
-    char path[IXLAND_MAX_PATH];
-    char interpreter[IXLAND_MAX_PATH];
+struct exec_image {
+    int type;
+    char path[MAX_PATH];
+    char interpreter[MAX_PATH];
 
     union {
         struct {
-            ixland_native_entry_t entry;
+            native_entry_t entry;
         } native;
         struct {
             void *module;
             void *instance;
         } wasi;
         struct {
-            char *interp_argv[IXLAND_MAX_ARGS];
+            char *interp_argv[TASK_MAX_ARGS];
             int interp_argc;
         } script;
     } u;
-} ixland_exec_image_t;
+};
 
-typedef enum {
-    IXLAND_TASK_RUNNING = 0,
-    IXLAND_TASK_INTERRUPTIBLE,
-    IXLAND_TASK_UNINTERRUPTIBLE,
-    IXLAND_TASK_STOPPED,
-    IXLAND_TASK_ZOMBIE,
-    IXLAND_TASK_DEAD
-} ixland_task_state_t;
-
-struct ixland_task {
+/* Task structure - Linux-style task_struct */
+struct task_struct {
+    /* PIDs */
     pid_t pid;
     pid_t ppid;
     pid_t tgid;
     pid_t pgid;
     pid_t sid;
 
+    /* State */
     atomic_int state;
     int exit_status;
     atomic_bool exited;
@@ -96,83 +103,93 @@ struct ixland_task {
     atomic_int stopsig;
     atomic_bool continued;
 
+    /* Thread and identity */
     pthread_t thread;
-    char comm[IXLAND_MAX_NAME];
-    char exe[IXLAND_MAX_PATH];
+    char comm[TASK_COMM_LEN];
+    char exe[MAX_PATH];
 
-    ixland_files_t *files;
-    ixland_fs_t *fs;
-    ixland_sighand_t *sighand;
-    ixland_tty_t *tty;
-    ixland_mm_emu_t *mm;
-    ixland_exec_image_t *exec_image;
+    /* Subsystem pointers */
+    struct files_struct *files;
+    struct fs_struct *fs;
+    struct sighand_struct *sighand;
+    struct tty_struct *tty;
+    struct mm_struct *mm;
+    struct exec_image *exec_image;
 
-    struct ixland_task *parent;
-    struct ixland_task *children;
-    struct ixland_task *next_sibling;
-    struct ixland_task *hash_next;
+    /* Process hierarchy */
+    struct task_struct *parent;
+    struct task_struct *children;
+    struct task_struct *next_sibling;
+    struct task_struct *hash_next;
 
-    /* Vfork tracking - set when created by vfork() */
-    struct ixland_task *vfork_parent;
+    /* Vfork tracking */
+    struct task_struct *vfork_parent;
 
+    /* Waiting */
     pthread_cond_t wait_cond;
     pthread_mutex_t wait_lock;
     int waiters;
 
+    /* Resources */
     struct rlimit rlimits[RLIMIT_NLIMITS];
 
+    /* Start time */
     struct timespec start_time;
 
+    /* Reference counting and locking */
     atomic_int refs;
     pthread_mutex_t lock;
 };
 
-/* Task table - exposed for tests */
+/* Task global table */
 extern pthread_mutex_t task_table_lock;
-extern ixland_task_t *task_table[IXLAND_MAX_TASKS];
+extern struct task_struct *task_table[TASK_MAX_TASKS];
 
-ixland_task_t *ixland_task_alloc(void);
-void ixland_task_free(ixland_task_t *task);
-ixland_task_t *ixland_current_task(void);
-void ixland_set_current_task(ixland_task_t *task);
+/* Task allocation */
+struct task_struct *alloc_task(void);
+void free_task(struct task_struct *task);
 
-pid_t ixland_alloc_pid(void);
-void ixland_free_pid(pid_t pid);
-void ixland_pid_init(void);
+/* Current task accessors */
+struct task_struct *get_current(void);
+void set_current(struct task_struct *task);
 
-int ixland_task_init(void);
-void ixland_task_deinit(void);
+/* PID allocation */
+pid_t alloc_pid(void);
+void free_pid(pid_t pid);
+void pid_init(void);
 
-ixland_task_t *ixland_task_lookup(pid_t pid);
+/* Task table management */
+int task_init(void);
+void task_deinit(void);
+struct task_struct *task_lookup(pid_t pid);
 int task_hash(pid_t pid);
 
-/* Process identity functions */
-pid_t ixland_getpid(void);
-pid_t ixland_getppid(void);
-pid_t ixland_getpgrp(void);
-pid_t ixland_getpgid(pid_t pid);
-int ixland_setpgid(pid_t pid, pid_t pgid);
-pid_t ixland_getsid(pid_t pid);
-pid_t ixland_setsid(void);
+/* Process identity */
+pid_t do_getpid(void);
+pid_t do_getppid(void);
+pid_t do_getpgrp(void);
+pid_t do_getpgid(pid_t pid);
+int do_setpgid(pid_t pid, pid_t pgid);
+pid_t do_getsid(pid_t pid);
+pid_t do_setsid(void);
 
-/* Fork functions */
-pid_t ixland_fork(void);
-int ixland_vfork(void);
+/* Fork/exec */
+pid_t do_fork(void);
+int do_vfork(void);
 
-/* Exit/wait functions */
-void ixland_exit(int status);
-void ixland__exit(int status);
-pid_t ixland_wait(int *wstatus);
-pid_t ixland_waitpid(pid_t pid, int *wstatus, int options);
-pid_t ixland_wait3(int *wstatus, int options, struct rusage *rusage);
-pid_t ixland_wait4(pid_t pid, int *wstatus, int options, struct rusage *rusage);
+/* Exit/wait */
+void do_exit(int status);
+void do_exit_group(int status);
+pid_t do_wait(int *wstatus);
+pid_t do_waitpid(pid_t pid, int *wstatus, int options);
+pid_t do_wait4(pid_t pid, int *wstatus, int options, struct rusage *rusage);
 
-/* Vfork notification helpers */
-void __ixland_vfork_exec_notify(void);
-void __ixland_vfork_exit_notify(void);
+/* Vfork notifications */
+void vfork_exec_notify(void);
+void vfork_exit_notify(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif /* TASK_H */
