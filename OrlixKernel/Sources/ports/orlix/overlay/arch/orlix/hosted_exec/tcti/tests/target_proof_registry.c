98d0dc4a0d8aaf7b20e9fb4bade674e7543039ad
Rudimar Ronsoni <rudimar@outlook.com>
2026-07-26 01:53:14 +0200

    fix(tcti): refresh scalar FP source provenance

--- OrlixKernel/Sources/ports/orlix/overlay/arch/orlix/hosted_exec/tcti/tests/target_proof_registry.c
+++ OrlixKernel/Sources/ports/orlix/overlay/arch/orlix/hosted_exec/tcti/tests/target_proof_registry.c
@@ -308,7 +308,7 @@ static const struct kunit_source_provenance kunit_sources[] = {
 	  "bb538a43b2e667be6118a6f457478aad3c03456fa6436879a5da83064dca09cc",
 	  "tcti_decode_test.o" },
 	{ SCALAR_FP_SOURCE,
-	  "c39f31899aa12ff965fe5a10942f87fd918182cc88bf614fdf195d28c107a029",
+	  "863706903667d712083874b4eb4376e8f8ce6cfa59d2feb87f29672a7867de37",
 	  "tcti_scalar_fp_semantics_test.o" },
 	{ ADVSIMD_FP_ARITHMETIC_SOURCE,
 	  "aab93c4152202c7347882f2dc77aeef666a63a2d409f5ab8c0aa7ebe453ebd9f",
